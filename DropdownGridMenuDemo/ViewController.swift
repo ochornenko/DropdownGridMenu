90631e070789ab9a26693c4b0d89a55ab41febdb
Oleg Chornenko <oleg.chornenko@gmail.com>
2017-11-25 01:34:14 -0500

    Implement demo presenting DropdownGridMenu

--- DropdownGridMenuDemo/ViewController.swift
+++ DropdownGridMenuDemo/ViewController.swift
@@ -10,16 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let items: [DropdownGridMenuItem] = [
+        DropdownGridMenuItem(text: "Retail", image: UIImage(named: "retail")!, selected: false),
+        DropdownGridMenuItem(text: "Handy", image: UIImage(named: "home_services")!, selected: false),
+        DropdownGridMenuItem(text: "Bars", image: UIImage(named: "nightlife")!, selected: false),
+        DropdownGridMenuItem(text: "Movers", image: UIImage(named: "movers")!, selected: false),
+        DropdownGridMenuItem(text: "Food", image: UIImage(named: "resturant_cafe")!, selected: false),
+        DropdownGridMenuItem(text: "Tech", image: UIImage(named: "creative_tech")!, selected: false),
+        DropdownGridMenuItem(text: "Auto", image: UIImage(named: "mechanic")!, selected: false),
+        DropdownGridMenuItem(text: "Desk", image: UIImage(named: "office_admin")!, selected: false),
+        DropdownGridMenuItem(text: "Hotel", image: UIImage(named: "hotel")!, selected: false),
+        DropdownGridMenuItem(text: "Tutor", image: UIImage(named: "tutoring")!, selected: false)
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(presentPopover(sender:)))
+        leftBarButtonItem.tintColor = UIColor.gray
+        self.navigationItem.leftBarButtonItem = leftBarButtonItem
+        
+        let rightBarButtonItem = UIBarButtonItem(image: UIImage(named: "menu"), style: UIBarButtonItemStyle.plain, target: self, action: #selector(presentMenu(sender:)))
+        self.navigationItem.rightBarButtonItem = rightBarButtonItem
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @objc func presentPopover(sender: UIBarButtonItem) {
+        let itemSize = CGSize(width: self.view.frame.size.width / 6, height: 70)
+        let contentSize = CGSize(width: self.view.frame.width / 1.5, height: self.view.frame.height / 2)
+        DropdownGridMenuController.presentPopover(self, appear: .fromTop, sender: sender, items: items, itemSize: itemSize, contentSize: contentSize, action: { item in
+            if item.isSelected {
+                print("item selected at index \(self.items.index(where: {$0 === item})!)")
+            } else {
+                print("item deselected at index \(self.items.index(where: {$0 === item})!)")
+            }
+        }, completion: {
+            print("completed")
+        } )
     }
-
-
+    
+    @objc func presentMenu(sender: UIBarButtonItem) {
+        let itemSize = CGSize(width: self.view.frame.size.width / 6, height: 70)
+        DropdownGridMenuController.presentFromViewController(self, appear: .fromRight, leftBarButtonItem: nil, rightBarButtonItem: sender, items: items, itemSize: itemSize, action: { item in
+            if item.isSelected {
+                print("item selected at index \(self.items.index(where: {$0 === item})!)")
+            } else {
+                print("item deselected at index \(self.items.index(where: {$0 === item})!)")
+            }
+        }, completion: {
+            print("completed")
+        } )
+    }
 }
-
